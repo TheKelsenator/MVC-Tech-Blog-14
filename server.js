1bc050f4668a79f33451029bb1bcb61aca1c1973
Kelsey O'Kelley <km.okelley89@gmail.com>
2023-04-16 16:41:38 -0600

    tyring to deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const ExpressHandlebars = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const routes = require('./controllers'); // /index.js?
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
@@ -39,9 +39,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-// app.listen(process.env.PORT || 3001);
-
-// sequelize.sync({ force: false }).then(() => {
 app.listen(PORT, () => {
   console.log('Now listening');
   sequelize.sync({ force: false })
